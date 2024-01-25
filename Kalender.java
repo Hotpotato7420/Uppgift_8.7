2206719756a0767fa62dc31e0754ef355779b168
Hotpotato7420 <118818818+Hotpotato7420@users.noreply.github.com>
2024-01-25 10:33:51 +0100

    Add files via upload
    Lade till uppgift 8.8

---
+++ Kalender.java
@@ -0,0 +1,56 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Kalender {
+    public static void main(String[] args) {
+        String s = datum();
+        System.out.println(s);
+    }
+
+    public static String datum(){
+        String datumNu = Calendar.getInstance().getTime().toString();
+        String år = datumNu.substring(24, 28);
+        String månad = datumNu.substring(4, 7);
+        String dag = datumNu.substring(8,11);
+        if (månad.contains("Jan")){
+            månad = "01";
+            if (månad.contains("Feb")) {
+                månad = "02";}
+
+            if (månad.contains("Mar")){
+                månad = "03";}
+
+            if (månad.contains("Apr")){
+                månad = "04";}
+
+            if (månad.contains("May")){
+                månad = "05";}
+
+            if (månad.contains("Jun")) {
+                månad = "06";}
+
+            if (månad.contains("Jul")){
+                månad = "07";}
+
+            if (månad.contains("Aug")){
+                månad = "08";}
+
+            if (månad.contains("Sep")){
+                månad = "09";}
+
+            if (månad.contains("Oct")) {
+                månad = "10";}
+
+            if (månad.contains("Nov")){
+                månad = "11";}
+
+            if (månad.contains("Dec")){
+                månad = "12";}
+        }
+
+        datumNu = år + "-" + månad + "-" + dag;
+        return datumNu;
+    }
+}
